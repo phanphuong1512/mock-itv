c019a21015e8cc82f1dae19c3e7a41af390aa6de
phanphuong1512 <phuongpv273@gmail.com>
2026-08-23 19:36:52 +0700

    fix(cloudflare): configure opennextjs build script and open-next.config.ts schema

--- frontend/open-next.config.ts
+++ frontend/open-next.config.ts
@@ -1,2 +1,26 @@
-// open-next.config.ts
-export default {};
+const config = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+};
+
+export default config;
